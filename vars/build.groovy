5bea7fc58d3bae9e3d4053ed45897882f89c9f5d
Reza Ghorbani <01ghorbani@gmail.com>
2023-09-08 21:30:52 +0330

    sdfsdf

--- vars/build.groovy
+++ vars/build.groovy
@@ -1,15 +1,10 @@
 import java.io.File
-
 def call(){
-File parsa = new File("./version.txt")
+    // File parsa = new File("./version.txt")
 
-old_version = parsa.text[0]
-new_version = old_version.toInteger() + 1
-parsa.text = new_version
-
-println new_version
+    // old_version = parsa.text[0]
+    // new_version = old_version.toInteger() + 1
+    // parsa.text = new_version
+    echo "this is test from function"
 }
-
-
-
 
